c8304e22875e3fe27984ccda3653fcfc7f95ad6c
Rafacodes8 <115218845+Rafacodes8@users.noreply.github.com>
2022-11-17 14:24:06 +0100

    .sql database file changes
    I added a new county table and new relations, also I changed the county_map table, unsure of the the use and implementation of the country_map.

--- Sql_file_database_Creation.sql
+++ Sql_file_database_Creation.sql
@@ -1,26 +1,37 @@
+DROP DATABASE logistics_company;
 
 CREATE DATABASE Logistics_Company;
 
 use Logistics_Company;	
 
+CREATE TABLE city
+(
+id int auto_increment primary key not null,
+city_name varchar(45) not null
+);
+
 CREATE TABLE truck
 (
 id int auto_increment primary key not null,
 capacity int not null,
-t_Status bool not null,
-city varchar (25)
+t_status bool not null,
+id_city int not null unique,
+CONSTRAINT FOREIGN KEY(id_city) REFERENCES city(id) ON DELETE NO ACTION ON UPDATE NO ACTION
 );
 
 CREATE TABLE driver
 (
-id int auto_increment primary key not null,
-driver_Name varchar(45) not null,
+id int auto_increment not null,
+driver_name varchar(45) not null,
 surname varchar(45) not null,
-personal_Number int(7) not null,
-working_Hours_Month int not null,
-driver_Status boolean not null,
-current_City varchar(45) default null,
-current_Truck varchar(7) default null
+personal_number int(7) not null,
+working_hours_month int not null,
+driver_status boolean not null,
+id_city int not null unique,
+id_truck int not null unique,
+PRIMARY KEY(id),
+CONSTRAINT FOREIGN KEY(id_truck) REFERENCES truck(id) ON DELETE NO ACTION ON UPDATE NO ACTION,
+CONSTRAINT FOREIGN KEY(id_city) REFERENCES city(id) ON DELETE NO ACTION ON UPDATE NO ACTION
 );
 
 CREATE TABLE logistic_order
@@ -34,17 +45,6 @@ CONSTRAINT FOREIGN KEY(id_truck) REFERENCES truck(id) ON DELETE NO ACTION ON UPD
 CONSTRAINT FOREIGN KEY(id_driver) REFERENCES driver(id) ON DELETE NO ACTION ON UPDATE NO ACTION
 );
 
-CREATE TABLE waypoint_list
-(
-id int auto_increment not null unique,
-city varchar(45) not null,
-cargo varchar(45) not null,
-action_Type varchar(6) not null,
-id_order int not null unique,
-PRIMARY KEY(id),
-CONSTRAINT FOREIGN KEY(id_order) REFERENCES logistic_order(id) ON DELETE NO ACTION ON UPDATE NO ACTION
-);
-
 CREATE TABLE cargo
 (
 id int auto_increment primary key not null,
@@ -54,9 +54,25 @@ weight_kg int not null,
 cargo_status varchar(9) default null
 );
 
+CREATE TABLE waypoint_list
+(
+id int auto_increment not null unique,
+cargo varchar(45) not null,
+action_type varchar(6) not null,
+id_order int not null unique,
+id_city int not null unique,
+id_cargo int not null unique,
+PRIMARY KEY(id),
+CONSTRAINT FOREIGN KEY(id_order) REFERENCES logistic_order(id) ON DELETE NO ACTION ON UPDATE NO ACTION,
+CONSTRAINT FOREIGN KEY(id_city) REFERENCES city(id) ON DELETE NO ACTION ON UPDATE NO ACTION,
+CONSTRAINT FOREIGN KEY(id_cargo) REFERENCES cargo(id) ON DELETE NO ACTION ON UPDATE NO ACTION
+);
+
+
 CREATE TABLE country_map
 (
 id int auto_increment primary key not null,
-city varchar(45) not null,
+departure varchar(45) not null,
+destination varchar(45) not null,
 distance int not null
 ); 
