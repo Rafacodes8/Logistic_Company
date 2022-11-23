19cbdf738f3172019a575771c03fef1df5176dae
Rafacodes8 <115218845+Rafacodes8@users.noreply.github.com>
2022-11-23 10:33:35 +0100

    Delete demo2 directory
    I should not commit the target folder

--- demo2/src/main/java/com/example/repositories/TruckRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-import com.example.entities.Truck;
-
-@Repository
-public interface TruckRepository extends CrudRepository<Truck, Integer> {
-}
