ec356bc06b783723f84854e9a7d1343f9eb9883d
Rafacodes8 <115218845+Rafacodes8@users.noreply.github.com>
2022-11-23 11:44:35 +0100

    Delete demo2 directory

--- demo2/src/main/java/com/example/repositories/CargoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-import com.example.entities.Cargo;
-
-@Repository
-public interface CargoRepository extends CrudRepository<Cargo, Integer> {
-}
--- demo2/src/main/java/com/example/repositories/CountryMapRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-import com.example.entities.CountryMap;
-
-@Repository
-public interface CountryMapRepository extends CrudRepository<CountryMap, Integer> {
-}
