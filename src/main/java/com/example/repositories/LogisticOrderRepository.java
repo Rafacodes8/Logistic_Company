c4164be0d6cf10ed2a43120baaeddfcd7aa67079
Rafacodes8 <115218845+Rafacodes8@users.noreply.github.com>
2022-12-22 11:28:18 +0100

    Delete src directory

--- src/main/java/com/example/repositories/LogisticOrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-import com.example.entities.LogisticOrder;
-
-@Repository
-public interface LogisticOrderRepository extends CrudRepository<LogisticOrder, Integer> {
-}
--- src/main/resources/static/css/actions.js
+++
@@ -1,3 +0,0 @@
-function funSiona() {
-    alert("funsiono!");
-}
